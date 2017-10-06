ec29030ba05d6d3b5d7615d6131856968462e34a
WestlakeStudentCouncil <josephjin7@gmail.com>
2017-10-05 21:35:01 -0500

    Adding Project Files

---
+++ 3rd Eye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  3rd Eye
+//
+//  Created by Joseph Jin on 10/5/17.
+//  Copyright © 2017 WestlakeAPC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
