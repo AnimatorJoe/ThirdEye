14c995db1cc13be679743d9b34ae477887f6c9dc
AnimatorJoe <josephjin7@gmail.com>
2017-10-09 20:39:11 -0500

    Added camera access and implemented mlmodels

--- 3rd Eye/ViewController.swift
+++ 3rd Eye/ViewController.swift
@@ -7,13 +7,77 @@
 //
 
 import UIKit
-import CoreML
+import AVKit
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    @IBOutlet var guessLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setCameraAccess()
+    }
+    
+    // Set up camera access
+    func setCameraAccess() {
+        // Set up capture session
+        let captureSession = AVCaptureSession()
+        //captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        // Show camera input to view
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.frame
+        
+        view.layer.addSublayer(previewLayer)
+        
+        view.addSubview(guessLabel)
+        
+        
+        // Extractiong and analysing image
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+        
+        //VNImageRequestHandler(cgImage: <#T##CGImage#>, options: []).perform()
+    }
+    
+    // Called everytime camera updates frame
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        //print("Capturing frame at \(Date())")
+        
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        guard let model = try? VNCoreMLModel(for: Resnet50().model) else { return }
+        
+        let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
+            
+            //print("Result: \(finishedReq.results!)")
+            
+            guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
+            
+            guard let firstObservation = results.first else { return }
+            
+            print(firstObservation.identifier, firstObservation.confidence)
+            
+            DispatchQueue.main.async {
+                self.guessLabel.text = "Guess: " + firstObservation.identifier + "\nConfidence:" + String(firstObservation.confidence * 100) + "%"
+            }
+            
+        }
+        
+        
+        
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
